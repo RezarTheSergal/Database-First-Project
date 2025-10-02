7a9a00ae8aa2332818525375790bb537696a3b58
drpepperus666 <obikaka159@gmail.com>
2025-10-02 17:34:48 +0300

    edit promptbox

--- frontend/shared/ui/PromptBox.py
+++ frontend/shared/ui/PromptBox.py
@@ -1,3 +1,4 @@
+"""
 from PySide6.QtWidgets import QMessageBox, QMainWindow
 from frontend.shared.ui import Widget, Modal
 from .const import YesButton, NoButton
@@ -10,3 +11,34 @@ class PromptBox(QMessageBox):
         self, parent: Widget | QMainWindow | Modal, title: str, description: str = ""
     ) -> QMessageBox.StandardButton:
         return self.question(parent, title, description, YesButton | NoButton, NoButton)
+"""
+
+from PySide6.QtWidgets import QMessageBox, QMainWindow
+from frontend.shared.ui import Widget, Modal
+from .const import YesButton, NoButton
+from backend.settings import ICON_PATH
+from .Icon import Icon
+
+class PromptBox(QMessageBox):
+    def __init__(self):
+        super().__init__()
+
+    def prompt(
+        self, parent: Widget | QMainWindow | Modal, title: str, description: str = ""
+    ):
+        # создание сообщения
+        msg = QMessageBox()
+        msg.setWindowTitle(title)
+        msg.setText(description)
+        msg.setStandardButtons(
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+        )
+
+        msg.setWindowIcon(Icon(ICON_PATH))
+
+        # замена текста кнопок на русский
+        msg.button(QMessageBox.StandardButton.Yes).setText("Да")
+        msg.button(QMessageBox.StandardButton.No).setText("Нет")
+
+        result = msg.exec()
+        return result
